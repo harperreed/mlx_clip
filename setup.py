062157fbd3c10c6e3ff7d7cb4ded37fe75bad580
Harper Reed <harper@nata2.org>
2024-03-26 21:08:25 -0500

    Incremented version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='mlx_clip',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     description='A simple package to use CLIP on apple silicon using the MLX libraries from Apple',
     long_description=open('README.md').read(),
